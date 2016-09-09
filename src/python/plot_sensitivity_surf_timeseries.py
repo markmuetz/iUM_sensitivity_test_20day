f8beb9e83ed70d47bf1af8e270f4058c37eae586
Mark Muetzelfeldt <markmuetz@gmail.com>
2016-09-09 13:10:29 +0100

    Modifications to plots.

--- src/python/plot_sensitivity_surf_timeseries.py
+++ src/python/plot_sensitivity_surf_timeseries.py
@@ -1,36 +1,40 @@
 from collections import OrderedDict as odict
+import importlib
 
-import numpy as np
-import pylab as plt
-import iris
+from processes import PylabProcess
+from stash import stash
 
-from omnium.processes import PylabProcess
-from omnium.stash import stash
-
-OPTS = odict([('Precip. flux equiv.', {'ylim': (0, 2000), 'convolve': True, 'yaxis': True}),
-             ('LHF', {'ylim': (0, 2000)}),
-             ('SHF', {'ylim': (0, 2000)})])
+OPTS = odict([('Precip. flux equiv.', {'ylim': (0, 1500), 'convolve': True, 'yaxis': True}),
+             ('LHF', {'ylim': (0, 1500)}),
+             ('SHF', {'ylim': (0, 1500)})])
 
 
-class PlotMultiTimeseries(PylabProcess):
+class PlotSensitivitySurfTimeseries(PylabProcess):
     name = 'plot_sensitivity_surf_timeseries'
     out_ext = 'png'
 
+    def load_modules(self):
+        self.np = importlib.import_module('numpy')
+        self.plt = importlib.import_module('pylab')
+        self.iris = importlib.import_module('iris')
+
     def load_upstream(self):
-        super(PlotMultiTimeseries, self).load_upstream()
+        super(PlotSensitivitySurfTimeseries, self).load_upstream()
         filenames = [n.filename(self.config) for n in self.node.from_nodes]
-        all_timeseries = iris.load(filenames)
+        all_timeseries = self.iris.load(filenames)
         self.data = all_timeseries
         return all_timeseries
 
     def run(self):
-        super(PlotMultiTimeseries, self).run()
+        super(PlotSensitivitySurfTimeseries, self).run()
         all_timeseries = self.data
-        fig, axes = plt.subplots(1, len(all_timeseries))
-        plt.subplots_adjust(wspace=0.32)
+        fig, axes = self.plt.subplots(1, len(all_timeseries))
+        fig.set_size_inches(PylabProcess.cm2inch(17, 7))
+        # self.plt.tight_layout()
+        self.plt.subplots_adjust(wspace=0.15, bottom=0.25)
         if len(all_timeseries) == 1:
             axes = [axes]
-        fig.suptitle('sensitivity surface timeseries ({})'.format(self.node.name))
+        # fig.suptitle('sensitivity surface timeseries ({})'.format(self.node.name))
         fig.canvas.set_window_title('timeseries')
         for i, timeseries in enumerate(all_timeseries):
             name, opt = OPTS.items()[i]
@@ -40,16 +44,25 @@ class PlotMultiTimeseries(PylabProcess):
 
             if 'convolve' in opt:
                 # Average over 12 hours for precip (dt is 10 min).
-                data = np.convolve(timeseries.data, np.ones((72, )) / 72., mode='same')
+                data = self.np.convolve(timeseries.data, self.np.ones((72, )) / 72., mode='same')
                 ax.plot(times / 24, data)
             else:
                 ax.plot(times / 24, timeseries.data)
             ax.set_xlabel('time (days)')
 
             if 'yaxis' in opt:
-                ax.set_ylabel(timeseries.units)
+                # ax.set_ylabel(timeseries.units)
+                ax.set_ylabel('W m$^{-2}$')
+            else:
+                ax.yaxis.set_ticklabels([])
             ax.set_title(name)
             ax.set_ylim(opt['ylim'])
         self.processed_data = fig
+
+    def save(self):
+        super(PylabProcess, self).save()
+        filename = self.node.filename(self.config)
+        self.plt.savefig(filename)
+        self.saved = True
 
 
